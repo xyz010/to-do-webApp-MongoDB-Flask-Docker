829950ed02f1ea1c9d2ec9ac35a6f09e156e9271
Andreas Loutzidis <aloutzidis@gmail.com>
2023-04-24 21:10:24 -0400

    updated indentation and corrected the /health and the intentional error

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from pymongo import MongoClient # Database connector
 from bson.objectid import ObjectId # For ObjectId to work
 from bson.errors import InvalidId # For catching InvalidId exception for ObjectId
 import os
+from flask import jsonify
+
 
 mongodb_host = os.environ.get('MONGO_HOST', 'localhost')
 mongodb_port = int(os.environ.get('MONGO_PORT', '27017'))
-client = MongoClient(mongodb_host, mongodb_port)
+# client = MongoClient(mongodb_host, mongodb_port)
+
+# this is the intentional error
+client = os.environ.get('MONGO_HOST', 'invalid-hostname')
+
+
 
 # client = MongoClient("mongodb://db:27017")#Configure the connection to the database
 
@@ -19,50 +26,52 @@ heading = "ToDo Reminder"
 #modify=ObjectId()
 
 def redirect_url():
-	return request.args.get('next') or \
-		request.referrer or \
-		url_for('index')
+    return request.args.get('next') or \
+        request.referrer or \
+        url_for('index')
 
 @app.route("/list")
 def lists ():
-	#Display the all Tasks
-	todos_l = todos.find()
-	a1="active"
-	return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
+    #Display the all Tasks
+    todos_l = todos.find()
+    a1="active"
+    return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
 
 @app.route("/")
+# @app.route("/test")
+
 @app.route("/uncompleted")
 def tasks ():
-	#Display the Uncompleted Tasks
-	# todos_l = todos.find({"done":"no"})
-	# intentional error to test the health monitoring
-	todos_l = todos.find({"testtesttest":"no"})
-	a2="active"
-	return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
+    #Display the Uncompleted Tasks
+    # todos_l = todos.find({"done":"no"})
+    # intentional error to test the health monitoring
+    todos_l = todos.find({"testtesttest":"no"})
+    a2="active"
+    return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
 
 
 @app.route("/completed")
 def completed ():
-	#Display the Completed Tasks
-	todos_l = todos.find({"done":"yes"})
-	a3="active"
-	return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
+    #Display the Completed Tasks
+    todos_l = todos.find({"done":"yes"})
+    a3="active"
+    return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
 
 @app.route("/done")
 def done ():
-	#Done-or-not ICON
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	if(task[0]["done"]=="yes"):
-		todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
-	else:
-		todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
-	redir=redirect_url()	# Re-directed URL i.e. PREVIOUS URL from where it came into this one
+    #Done-or-not ICON
+    id=request.values.get("_id")
+    task=todos.find({"_id":ObjectId(id)})
+    if(task[0]["done"]=="yes"):
+        todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
+    else:
+        todos.update_one({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
+    redir=redirect_url()	# Re-directed URL i.e. PREVIOUS URL from where it came into this one
 
 #	if(str(redir)=="http://localhost:5000/search"):
 #		redir+="?key="+id+"&refer="+refer
 
-	return redirect(redir)
+    return redirect(redir)
 
 #@app.route("/add")
 #def add():
@@ -70,67 +79,74 @@ def done ():
 
 @app.route("/action", methods=['POST'])
 def action ():
-	#Adding a Task
-	name=request.values.get("name")
-	desc=request.values.get("desc")
-	date=request.values.get("date")
-	pr=request.values.get("pr")
-	todos.insert_one({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})
-	return redirect("/list")
+    #Adding a Task
+    name=request.values.get("name")
+    desc=request.values.get("desc")
+    date=request.values.get("date")
+    pr=request.values.get("pr")
+    todos.insert_one({ "name":name, "desc":desc, "date":date, "pr":pr, "done":"no"})
+    return redirect("/list")
 
 @app.route("/remove")
 def remove ():
-	#Deleting a Task with various references
-	key=request.values.get("_id")
-	todos.delete_one({"_id":ObjectId(key)})
-	return redirect("/")
+    #Deleting a Task with various references
+    key=request.values.get("_id")
+    todos.delete_one({"_id":ObjectId(key)})
+    return redirect("/")
 
 @app.route("/update")
 def update ():
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	return render_template('update.html',tasks=task,h=heading,t=title)
+    id=request.values.get("_id")
+    task=todos.find({"_id":ObjectId(id)})
+    return render_template('update.html',tasks=task,h=heading,t=title)
 
 @app.route("/action3", methods=['POST'])
 def action3 ():
-	#Updating a Task with various references
-	name=request.values.get("name")
-	desc=request.values.get("desc")
-	date=request.values.get("date")
-	pr=request.values.get("pr")
-	id=request.values.get("_id")
-	todos.update_one({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
-	return redirect("/")
+    #Updating a Task with various references
+    name=request.values.get("name")
+    desc=request.values.get("desc")
+    date=request.values.get("date")
+    pr=request.values.get("pr")
+    id=request.values.get("_id")
+    todos.update_one({"_id":ObjectId(id)}, {'$set':{ "name":name, "desc":desc, "date":date, "pr":pr }})
+    return redirect("/")
 
 @app.route("/search", methods=['GET'])
 def search():
-	#Searching a Task with various references
+    #Searching a Task with various references
 
-	key=request.values.get("key")
-	refer=request.values.get("refer")
-	if(refer=="id"):
-		try:
-			todos_l = todos.find({refer:ObjectId(key)})
-			if not todos_l:
-				return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading,error="No such ObjectId is present")
-		except InvalidId as err:
-			pass
-			return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading,error="Invalid ObjectId format given")
-	else:
-		todos_l = todos.find({refer:key})
-	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
+    key=request.values.get("key")
+    refer=request.values.get("refer")
+    if refer== "id":
+        try:
+            todos_l = todos.find({refer:ObjectId(key)})
+            if not todos_l:
+                return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading,error="No such ObjectId is present")
+        except InvalidId as err:
+            pass
+            return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading,error="Invalid ObjectId format given")
+    else:
+        todos_l = todos.find({refer:key})
+    return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
 
 @app.route("/about")
 def about():
-	return render_template('credits.html',t=title,h=heading)
+    return render_template('credits.html',t=title,h=heading)
+
 @app.route("/health")
-def health_monitoring():
-	return "Everything is healthy!", 200
+def health():
+    try:
+        # Check if the database connection is working by pinging it
+        client.admin.command('ping')
+        return jsonify({"status": "OK"}), 200
+    except Exception as e:
+        return jsonify({"status": "ERROR", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
-	env = os.environ.get('FLASK_ENV', 'development')
-	port = int(os.environ.get('PORT', 5000))
-	debug = False if env == 'production' else True
-	app.run(port=port, debug=debug)
+    env = os.environ.get('FLASK_ENV', 'development')
+    port = int(os.environ.get('PORT', 5000))
+    debug = False if env == 'production' else True
+    app.run(port=port, debug=debug)
 
 # Careful with the debug mode..
